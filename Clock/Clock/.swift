9b876da57c7322940fe12f1731dab919964739ea
Hannu Packalen <hpackalen@lcs.on.ca>
2024-11-11 11:43:03 -0500

    Project orginization complete. Changed name of view.

---
+++ Clock/Clock/.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Hannu Packalen on 2024-11-11.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clock/Clock/Views/LandingView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clock
+//
+//  Created by Hannu Packalen on 2024-11-11.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    LandingView()
+}

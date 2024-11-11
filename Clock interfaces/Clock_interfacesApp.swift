43cf4a41117931df40b1d4d9c5df72264ff36776
Hannu Packalen <hpackalen@lcs.on.ca>
2024-11-11 11:35:53 -0500

    Initial Commit

---
+++ Clock interfaces/Clock_interfacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clock_interfacesApp.swift
+//  Clock interfaces
+//
+//  Created by Hannu Packalen on 2024-11-11.
+//
+
+import SwiftUI
+
+@main
+struct Clock_interfacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clock interfaces/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clock interfaces
+//
+//  Created by Hannu Packalen on 2024-11-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
